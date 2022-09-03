d3dca2028f253ed32ff8127ab98bab446052d0c8
Salihler <eren.salihler@gmail.com>
2022-09-04 00:49:29 +0300

    album-is-working

--- like-disc.Core/Services/ISpotifyService.cs
+++ like-disc.Core/Services/ISpotifyService.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace like_disc.Core.Services
 {
-    public interface ISpotifyService<IEntity>
+    public interface ISpotifyService<TEntity> where TEntity : class
     {
-        public Task<IEntity> Get(string param, Token token);
+        public Task<TEntity> Get(string param, Token token);
     }
 }
--- like-disc.Service/Services/HttpServices/SpotifyService.cs
+++ like-disc.Service/Services/HttpServices/SpotifyService.cs
@@ -1,27 +1,40 @@
 ﻿using like_disc.Core.Contexts.HttpClients;
 using like_disc.Core.Models;
 using like_disc.Core.Services;
+using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace like_disc.Service.Services.HttpServices
 {
-    public class SpotifyService<TEntity> : ISpotifyService<TEntity> where TEntity : class
+    public class SpotifyService<TEntity> : SpotifyClient, ISpotifyService<TEntity> where TEntity : class
     {
         private readonly HttpClient _client;
 
-        public SpotifyService(HttpClient httpClient)
+        public SpotifyService(HttpClient httpClient) : base(httpClient)
         {
             _client = httpClient;
-            _client.BaseAddress = new Uri("https://api.spotify.com/v1/");
-            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         public async Task<TEntity> Get(string param, Token token)
         {
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Access_Token); 
-            
-            return await _client.GetFromJsonAsync<TEntity>(nameof(TEntity) + $"/{param}");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Access_Token);
+
+            var response = await _client.GetAsync(new Uri(_client.BaseAddress, $"{typeof(TEntity).Name.ToLower()}s/{param}"));
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var stream = response.Content.ReadAsStringAsync())
+                {
+                    if (stream.IsCompletedSuccessfully)
+                    {
+                        TEntity album = JsonConvert.DeserializeObject<TEntity>(stream.Result);
+                        return album;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
